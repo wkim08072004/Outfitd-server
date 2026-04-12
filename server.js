46e603f0cb0a0eeadd1ee077d98abdf86e79f11c
Wilson Kim <wkim08072004@gmail.com>
2026-04-11 22:33:32 -0700

    fix content-type cors header

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://outfitd.co");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-res.header("Access-Control-Allow-Headers", "*");
+res.header(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+);
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
