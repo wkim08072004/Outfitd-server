21853af6d848712b0d0e6ee94005c48247f0d2dd
Wilson Kim <wkim08072004@gmail.com>
2026-04-11 23:01:01 -0700

    fix cors headers properly

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
 res.header(
   "Access-Control-Allow-Headers",
-  req.headers["access-control-request-headers"] || "*"
+  "Content-Type, Authorization, X-Requested-With"
 );
 
   if (req.method === "OPTIONS") {
