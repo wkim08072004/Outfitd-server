591bb5412c64a3eda47103425dca044cfab6c0bd
Wilson Kim <wkim08072004@gmail.com>
2026-04-11 22:30:10 -0700

    allow all cors headers

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://outfitd.co");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+res.header("Access-Control-Allow-Headers", "*");
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
